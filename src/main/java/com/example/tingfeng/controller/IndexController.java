487155b6b8cf3133e7bdc293a84f1246bf0497bf
听风 <tingfeng@helijia.com>
2019-11-05 10:43:07 +0800

    index

--- src/main/java/com/example/tingfeng/controller/IndexController.java
+++ src/main/java/com/example/tingfeng/controller/IndexController.java
@@ -3,6 +3,10 @@ package com.example.tingfeng.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Package com.example.tingfeng.controller
  * @Description
@@ -13,7 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "嗨，你好😊。";
     }
+
+    /**
+     * 测试慢执行
+     */
+    @RequestMapping("/hello/cycle")
+    public String helloCycle(String stop) throws InterruptedException {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        for (int i = 0; i < 10000; i++) {
+            TimeUnit.MILLISECONDS.sleep(10);
+            System.out.println(i + " 嗨，你好 " + LocalDateTime.now().format(dtf));
+        }
+        return "hello cycle over";
+    }
+
+    /**
+     * 测试异常
+     */
+    @RequestMapping("/division")
+    public String division() {
+        System.out.println(1 / 0);
+        return "division over";
+    }
+
+
 }
